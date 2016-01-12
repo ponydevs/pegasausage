36fe76fb797a17cdae23751e34b49ecf4c725a25
techniponi <wincam97@gmail.com>
2016-01-12 13:18:30 -0600

    Improved garbage collection, added difficulty

--- pegasausage.js
+++ pegasausage.js
@@ -28,14 +28,23 @@ var diffs = [
 		hp: 2,
 		cloudAmount: 80
 	},
-	
-	twf = {
-		name: "Together We've Fallen",
+
+	apocalypse = {
+		name: "Apocalypse",
 		generationRate: 7,
 		playerSpeed: 0.08,
 		cloudSpeed: 0.8,
 		hp: 2,
 		cloudAmount: 100
+	},
+	
+	twf = {
+		name: "Together We've Fallen",
+		generationRate: 1,
+		playerSpeed: 0.12,
+		cloudSpeed: 5.6,
+		hp: 1,
+		cloudAmount: 700
 	}
 
 ]
@@ -55,6 +64,22 @@ var gameState = 2;
 
 var audio = document.getElementById('audio');
 var hit = document.getElementById('hit');
+
+document.body.addEventListener('touchmove', function (event) {
+	event.preventDefault();
+}, false);
+
+window.mobileAndTabletcheck = function () {
+	var check = false;
+	(function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true })(navigator.userAgent || navigator.vendor || window.opera);
+	return check;
+}
+
+var textSize;
+
+if (window.mobileAndTabletcheck()) {
+	textSize = 8;
+}
 			
 // random int in range - [min, max]
 function getRandomInt(min, max) {
@@ -108,8 +133,8 @@ scene.add(Player);
 var charImg = new THREE.TextureLoader().load('char.png');
 var char = new THREE.Mesh(
 	new THREE.PlaneGeometry(0.5, 0.5, 0),
-	new THREE.MeshBasicMaterial({map: charImg})
-);
+	new THREE.MeshBasicMaterial({ map: charImg })
+	);
 char.material.transparent = true;
 Player.add(char);
 char.position.set(0, -0.15, -0.8);
@@ -124,12 +149,12 @@ var psLogoImg = new THREE.TextureLoader().load('title.png');
 var tpLogo = new THREE.Mesh(
 	new THREE.PlaneGeometry(12, 1),
 	new THREE.MeshBasicMaterial({ map: tpLogoImg })
-);
+	);
 
 var psLogo = new THREE.Mesh(
 	new THREE.PlaneGeometry(12, 2),
 	new THREE.MeshBasicMaterial({ map: psLogoImg })
-);
+	);
 
 Player.add(tpLogo);
 Player.add(psLogo);
@@ -141,31 +166,37 @@ psLogo.position.set(0, 4, -10);
 var pvfmText = document.createElement('div');
 pvfmText.style.position = 'absolute';
 pvfmText.innerHTML = "Game created by Techniponi<br/>Music from <a href='http://ponyvillefm.com'>Ponyville FM</a>";
-pvfmText.style.top = window.innerHeight - 50 +'px';
-pvfmText.style.left = 20+'px';
+pvfmText.style.top = window.innerHeight - 50 + 'px';
+pvfmText.style.left = 20 + 'px';
+if (textSize != undefined) {
+	pvfmText.style.fontSize = textSize + 'pt';
+}
 document.body.appendChild(pvfmText);
 
 var text2 = document.createElement('div');
 text2.style.position = 'absolute';
 text2.style.textAlign = "center";
-text2.innerHTML = "HP: " + hp + "<br/>Distance: " + Math.floor(distance * cloudSpeed);
+text2.innerHTML = "HP: " + hp + "<br/>Distance: " + Math.floor(distance);
 text2.style.top = 20 + 'px';
 text2.style.left = (window.innerWidth / 2) - text2.offsetWidth / 2 + 'px';
+if (textSize != undefined) {
+	text2.style.fontSize = textSize + 'pt';
+}
 document.body.appendChild(text2);
 
 // init base objects
 var meshes = [];
 var cloudTexture = new THREE.TextureLoader().load('sprite.png');
+var cloudMaterial = new THREE.MeshBasicMaterial({ map: cloudTexture });
+cloudMaterial.transparent = true;
+var cloudplaneGeometry = new THREE.PlaneGeometry(1, 1);
 
 // create a new iteration of clouds
 function generateSprites() {
 
 	for (i = 0; i < cloudAmount; i++) {
-		var material = new THREE.MeshBasicMaterial({ map: cloudTexture });
-		material.transparent = true;
-		var planeGeometry = new THREE.PlaneGeometry(1, 1);
-		var mesh = new THREE.Mesh(planeGeometry, material);
-		mesh.position.set(getRandomInt(-10, 10), getRandomInt(-10, 10), getRandomInt(-35, -40));
+		var mesh = new THREE.Mesh(cloudplaneGeometry, cloudMaterial);
+		mesh.position.set(getRandomArbitrary(-10, 10), getRandomArbitrary(-10, 10), getRandomArbitrary(-35, -40));
 
 		meshes.push(mesh);
 
@@ -199,7 +230,7 @@ function resetGame() {
 	framesPassed = 0;
 	distance = 0;
 	hp = difficulty.hp;
-	text2.innerHTML = "HP: " + hp + "<br/>Distance: " + Math.floor(distance * cloudSpeed);
+	text2.innerHTML = "HP: " + hp + "<br/>Distance: " + Math.floor(distance);
 	text2.style.top = 20 + 'px';
 	text2.style.left = (window.innerWidth / 2) - text2.offsetWidth / 2 + 'px';
 	gameState = 1;
@@ -212,18 +243,29 @@ function resetGame() {
 	tpLogo.visible = false;
 	psLogo.visible = false;
 	char.visible = true;
-	camera.position.set(0,0,0);
+	camera.position.set(0, 0, 0);
 	char.position.set(0, -0.2, -0.8);
 	char.lookAt(poo);
-}			
+}
 
 var framesPassed = 0;
+var charPos = new THREE.Vector3(0, -0.2, -0.8);
 
 // runs every frame
 function render() {
 	requestAnimationFrame(render);
 
-	framesPassed ++;
+	framesPassed++;
+
+	if (framesPassed >= 120 && framesPassed < 360) {
+		char.position.z += 0.01;
+		char.position.y -= 0.02;
+	}
+
+	if (framesPassed >= 360) {
+		char.visible = false;
+		char.position = charPos;
+	}
 
 	if (input.isDown("P")) {
 		pkeyDown = true;
@@ -238,18 +280,9 @@ function render() {
 
 		pkeyDown = false;
 	}
-	
-	if((framesPassed / 900) % 1 == 0 ){
-		cloudAmount *= 1.5;
-		cloudSpeed *= 1.5;
-		generationRate = Math.floor(generationRate * 0.75);
-		if(generationStep >= generationRate){
-			generationStep = 0;
-		}
-	}
 
 	tpLogo.lookAt(Player.position);
-	char.position.set(0, -0.2, -0.8);
+	char.position = charPos;
 
 	renderer.autoClear = false;
 	renderer.clear();
@@ -269,6 +302,15 @@ function render() {
 	if (gameState == 1) {
 
 		distance++;
+
+		if ((framesPassed / 900) % 1 == 0) {
+			cloudAmount *= 1.5;
+			cloudSpeed *= 1.5;
+			generationRate = Math.floor(generationRate * 0.75);
+			if (generationStep >= generationRate) {
+				generationStep = 0;
+			}
+		}
 
 		// movement
 		if (input.isDown("W") && Player.position.y <= movementRange) {
@@ -299,14 +341,17 @@ function render() {
 				x.position.z += cloudSpeed;
 
 				if (x.position.distanceTo(Player.position) < 0.5) {
-					scene.remove(x);
-					meshes.splice(meshes.indexOf(x), 1);
 					collisionNumber++;
 					console.log(collisionNumber);
 					hp--;
 					shaking = true;
 					hit.currentTime = 0;
 					hit.play();
+					meshes.splice(meshes.indexOf(x), 1);
+					scene.remove(x);
+					cloudMaterial.dispose();
+					cloudplaneGeometry.dispose();
+					cloudTexture.dispose();
 				}
 			}
 		});
@@ -319,7 +364,7 @@ function render() {
 				camera.position.z = 0;
 				shaking = false;
 				shakeStep = 0;
-				char.position.set(0, -0.2, -0.8);
+				char.position = charPos;
 			} else {
 				camera.position.x = getRandomArbitrary(0, 0.5);
 				camera.position.y = getRandomArbitrary(0, 0.5);
@@ -327,7 +372,7 @@ function render() {
 			}
 		}
 
-		text2.innerHTML = "HP: " + hp + "<br/>Distance: " + Math.floor(distance * cloudSpeed);
+		text2.innerHTML = "HP: " + hp + "<br/>Distance: " + Math.floor(distance);
 
 	} else if (gameState == 0) {
 		// Reset game to nothing
@@ -344,7 +389,7 @@ function render() {
 		Player.position.y = 0;
 		Player.position.z = 0;
 
-		text2.innerHTML = "GAME OVER<br/>Distance: " + Math.floor(distance * cloudSpeed) + "<br/><br/>Press R to restart!<br/><br/>Difficulty: " + difficulty.name + "<br/>Press T to cycle difficulty.";
+		text2.innerHTML = "GAME OVER<br/>Distance: " + Math.floor(distance) + "<br/><br/>Press R to restart!<br/><br/>Difficulty: " + difficulty.name + "<br/>Press T to cycle difficulty.";
 
 		text2.style.top = (window.innerHeight / 2) - text2.offsetHeight / 2 + 'px';
 		text2.style.left = (window.innerWidth / 2) - text2.offsetWidth / 2 + 'px';
@@ -377,6 +422,7 @@ function render() {
 
 		if (input.isDown("R")) {
 			resetGame();
+			drawing = false;
 		}
 
 		if (input.isDown("T")) {
